c781dcdb39d6f6d64fecca70c0c3e42e292be37c
sherin0782 <sherin0782@gmail.com>
2019-04-23 16:27:42 +0530

    Angular commit

---
+++ Class 1/productapp/src/app/products/products.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import { template } from '@angular/core/src/render3';
+
+@Component({
+selector:'app-products',
+// template:`<div class="panel panel-default">
+// <div class="panel-body">Hello World</div>
+// </div>`
+templateUrl:'./products.component.html'
+})
+
+export class productsComponent{
+
+}
